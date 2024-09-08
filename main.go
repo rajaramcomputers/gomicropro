311cdc2429ad7941321494e100fc8ea3708e202b
rajaramcomputers <rajaramcomputers@gmail.com>
2024-09-08 22:01:05 +0400

    to continue static content

--- main.go
+++ main.go
@@ -8,19 +8,33 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Customer Service")
-	})
+
+	http.Handle("/", myHandler("Customer New Handler"))
 
 	s := http.Server{
 		Addr: ":3000",
 	}
 	go func() {
-		log.Fatal(s.ListenAndServeTLS("./cert.pem", "./key.pem"))
+		log.Fatal(s.ListenAndServe())
 	}()
 
 	fmt.Println("Server started and listening on :3000 <Enter> to shutdown")
 	fmt.Scanln()
 	s.Shutdown(context.Background())
 	fmt.Println("Server stopped")
+}
+
+type myHandler string
+
+func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("X-Powered-By", "Gomicropro")
+	http.SetCookie(w, &http.Cookie{
+		Name:   "session",
+		Value:  "1234567890",
+		MaxAge: 3600,
+	})
+	w.WriteHeader(http.StatusAccepted)
+	fmt.Fprintln(w, string(mh))
+	fmt.Fprintln(w, r.Header)
+
 }
